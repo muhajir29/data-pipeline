cf3d0ea9f2b5f2835a68544e56e7436e4474b930
Imam Muhajir <39195682+muhajir29@users.noreply.github.com>
2024-09-10 11:00:59 +0000

    completed

---
+++ orchestrator/datapipeline/transformers/dbt_transformation.py
@@ -0,0 +1,11 @@
+import os
+
+if 'transformer' not in globals():
+    from mage_ai.data_preparation.decorators import transformer
+if 'test' not in globals():
+    from mage_ai.data_preparation.decorators import test
+
+
+@transformer
+def transform(data, *args, **kwargs):
+    os.system("dbt build")
